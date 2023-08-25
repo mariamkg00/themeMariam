86d308f2ddd320098a941c2c6713345c2b34974d
mariamkg00 <mariamkhadijah00@gmail.com>
2023-08-25 11:09:34 -0700

    Initial commit

--- R/theme_albondigas.R
+++ R/theme_albondigas.R
@@ -8,5 +8,6 @@ theme_albondigas <- function(){
   theme(plot.background = element_rect(fill = "magenta"),
         panel.background = element_rect(fill = "grey"),
         axis.text = element_text(color = "yellow"),
-        panel.grid = element_line(color = "cyan"))
+        panel.grid = element_line(color = "cyan"),
+        panel.grid.major = element_line(color = "green"))
 }
