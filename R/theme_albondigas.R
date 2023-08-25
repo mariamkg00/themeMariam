b9cf509c6204fc3aac3c9089cb32c82133bf008e
mariamkg00 <mariamkhadijah00@gmail.com>
2023-08-25 10:58:59 -0700

    Initial commit

---
+++ R/theme_albondigas.R
@@ -0,0 +1,12 @@
+#' An eighties inspired ggplot theme
+#'
+#' @return
+#' @export
+#'
+#' @examples
+theme_albondigas <- function(){
+  theme(plot.background = element_rect(fill = "magenta"),
+        panel.background = element_rect(fill = "grey"),
+        axis.text = element_text(color = "yellow"),
+        panel.grid = element_line(color = "cyan"))
+}
